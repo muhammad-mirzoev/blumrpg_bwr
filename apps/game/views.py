abe4f6a0e7fb50b941ac3ed32b76743ae1fbd7e9
mac <237774366+shumsterm406-oss@users.noreply.github.com>
2026-08-27 18:46:42 +0600

    added views for "game/" to templates

--- apps/game/views.py
+++ apps/game/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class PlayView(LoginRequiredMixin, TemplateView):
+    """
+    Отдаёт HTML-обёртку игровой сцены. Весь геймплей - на JS/Canvas,
+    Django здесь только рендерит шаблон и требует авторизацию.
+    Если пользователь не залогинен - редирект на LOGIN_URL (см. settings.py).
+    """
+    template_name = 'game/play.html'
