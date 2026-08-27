e70993a51eb8284053086c426fbfdc67f9aae150
mac <237774366+shumsterm406-oss@users.noreply.github.com>
2026-08-27 17:23:12 +0600

    added views for "characters"

--- apps/characters/views.py
+++ apps/characters/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Character
+from .serializers import CharacterSerializer
 
-# Create your views here.
+
+class MyCharacterView(generics.RetrieveAPIView):
+    """
+    GET /api/character/
+    Возвращает персонажа текущего залогиненного пользователя.
+    """
+    serializer_class = CharacterSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.character
