18bfff95e840f4a9b64713a4aad22c44372c7bf0
mac <237774366+shumsterm406-oss@users.noreply.github.com>
2026-08-27 17:34:10 +0600

    added apps.py (def ready) for "characters"

--- apps/characters/apps.py
+++ apps/characters/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CharactersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.characters'
+
+    def ready(self):
+        import apps.characters.signals  # noqa: F401
