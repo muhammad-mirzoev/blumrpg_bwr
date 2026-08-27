09b269b809281a5d8c227d82ac1629310ae2a30c
mac <237774366+shumsterm406-oss@users.noreply.github.com>
2026-08-27 17:38:52 +0600

    add urls for "characters" to config

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('apps.accounts.urls')),
+    path('api/character/', include('apps.characters.urls')),
 ]
 
 if settings.DEBUG:
