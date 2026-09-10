4e835c4adba955210f023bde0411ff5052b46d5e
mac <237774366+shumsterm406-oss@users.noreply.github.com>
2026-09-10 20:05:57 +0600

    feat(items): register items inventory and equipment in admin

--- apps/items/admin.py
+++ apps/items/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Item, InventoryItem, Equipment
 
-# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'item_type', 'damage_bonus', 'defense_bonus', 'heal_amount', 'is_stackable')
+    list_filter = ('item_type', 'is_stackable')
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(InventoryItem)
+class InventoryItemAdmin(admin.ModelAdmin):
+    list_display = ('character', 'item', 'quantity', 'acquired_at')
+    list_filter = ('item__item_type',)
+    search_fields = ('character__name', 'item__name')
+    autocomplete_fields = ('character', 'item')
+
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('character', 'weapon', 'armor', 'helmet', 'boots')
+    search_fields = ('character__name',)
+    autocomplete_fields = ('character', 'weapon', 'armor', 'helmet', 'boots')
